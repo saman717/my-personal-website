9bbf8b49a05c22ea43cf32c26b974888dcd451e7
saman717 <mohamad.khoshnood.10@gmail.com>
2026-06-02 22:22:36 +0330

    menu fixed

--- src/app/[locale]/admin/layout.tsx
+++ src/app/[locale]/admin/layout.tsx
@@ -48,7 +48,7 @@ export default function AdminLayout({
                                 : "-translate-x-false"
                         }`}
                 >
-                    {/* <Sidebar locale={locale} /> */}
+                    <Sidebar locale={locale} />
                 </div>
 
                 {/* content */}
